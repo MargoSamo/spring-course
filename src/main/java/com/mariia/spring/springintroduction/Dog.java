1fad6e25ceaa8be773f71af30d2b843af1aa74e6
MargoSamo <margo_ii@mail.ru>
2022-08-29 17:11:24 +0400

    new project spring

---
+++ src/main/java/com/mariia/spring/springintroduction/Dog.java
@@ -0,0 +1,7 @@
+package com.mariia.spring.springintroduction;
+
+public class Dog {
+    public void say() {
+        System.out.println("Bow-Wow");
+    }
+}
---
+++ src/test/java/com/mariia/spring/springintroduction/DogTest.java
@@ -0,0 +1,9 @@
+package com.mariia.spring.springintroduction;
+
+public class DogTest {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.say();
+    }
+
+}
